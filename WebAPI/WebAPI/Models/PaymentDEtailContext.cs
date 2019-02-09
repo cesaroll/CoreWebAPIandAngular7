82f72e9811630bdca5827c386534ca004cb68e22
Cesar Lopez <cesarl@outlook.com>
2019-02-08 17:28:16 -0700

    Adding Context, model, migration and updating DB

--- WebAPI/WebAPI/Models/PaymentDEtailContext.cs
+++ WebAPI/WebAPI/Models/PaymentDEtailContext.cs
@@ -2,10 +2,17 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
 
 namespace WebAPI.Models
 {
-    public class PaymentDEtailContext
+    public class PaymentDetailContext : DbContext
     {
+        public PaymentDetailContext(DbContextOptions<PaymentDetailContext> options) : base(options)
+        {
+            
+        }
+
+        public DbSet<PaymentDetail> DbSet { get; set; }
     }
 }
